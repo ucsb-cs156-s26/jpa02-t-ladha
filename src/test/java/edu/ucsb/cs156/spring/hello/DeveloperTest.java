8d99bd49ef1925e21d866111e13f0b0c40ed59b0
Tanvi Ladha <tanvil@169-231-110-12.wireless.ucsb.edu>
2026-04-08 17:09:49 -0700

    TL - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -28,5 +28,8 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("t-ladha", Developer.getGithubId());
+    }
 }
